13f63254056f1e6bfb3dad29a57e227025882ea3
robwschlegel <robwschlegel@gmail.com>
2021-01-15 13:45:35 +0100

    Expanded on the GLODAP data extraction to now include more carbonate chemistry variables

--- code/data_processing.R
+++ code/data_processing.R
@@ -21,35 +21,53 @@ ncdump::NetCDF("~/pCloudDrive/Data/GLODAPv2.2016b_MappedClimatologies/GLODAPv2.2
 GLODAP_Arctic_bottle <- read_csv("~/pCloudDrive/Data/GLODAPv2.2020_Arctic_Ocean.csv") %>% 
   `colnames<-`(sub("G2", "", colnames(.))) %>% 
   mutate(t = as.Date(paste(year, month, day, sep = "-"))) %>% 
-  dplyr::rename(lon = longitude, lat = latitude, TCO2 = tco2) %>% 
-  dplyr::select(lon, lat, depth, t, TCO2)
+  dplyr::rename(lon = longitude, lat = latitude, TCO2 = tco2, TAlk = talk) %>% 
+  dplyr::select(lon, lat, depth, t, TCO2, TAlk, salinity, temperature) %>% 
+  replace(is.na(.), NA)
 
-# Bottle data averaged over all collections
+# Bottle data averaged over all sites/CTD stations
 GLODAP_Arctic_bottle_mean <- GLODAP_Arctic_bottle %>% 
   group_by(lon, lat, depth) %>% 
-  summarise(TCO2 = mean(TCO2, na.rm = T), .groups = "drop") %>% 
-  mutate(TCO2 = ifelse(is.na(TCO2), NA, TCO2))
+  summarise_all(mean, na.rm = T, .groups = "drop") %>%
+  # mutate(TCO2 = ifelse(is.na(TCO2), NA, TCO2))
+  replace(is.na(.), NA)
 
 # NetCDF layers
-GLODAP_depth <- tidync("~/pCloudDrive/Data/GLODAPv2.2016b_MappedClimatologies/GLODAPv2.2016b.TCO2.nc") %>% 
-  activate("D2") %>% 
-  hyper_tibble() %>% 
+GLODAP_depth <- tidync::tidync("~/pCloudDrive/Data/GLODAPv2.2016b_MappedClimatologies/GLODAPv2.2016b.TCO2.nc") %>% 
+  tidync::activate("D2") %>% 
+  tidync::hyper_tibble() %>% 
   dplyr::rename(depth = Depth)
 GLODAP_TCO2_grid <- tidync("~/pCloudDrive/Data/GLODAPv2.2016b_MappedClimatologies/GLODAPv2.2016b.TCO2.nc") %>% 
+  hyper_tibble() %>%
+  dplyr::select(lon, lat, depth_surface, TCO2)
+GLODAP_TAlk_grid <- tidync("~/pCloudDrive/Data/GLODAPv2.2016b_MappedClimatologies/GLODAPv2.2016b.TAlk.nc") %>% 
   hyper_tibble() %>% 
-  left_join(GLODAP_depth, by = c("depth_surface")) %>% 
-  dplyr::select(lon, lat, depth, TCO2) %>% 
-  mutate(lon = ifelse(lon >180, lon-360, lon))
+  dplyr::select(lon, lat, depth_surface, TAlk)
+GLODAP_sal_grid <- tidync("~/pCloudDrive/Data/GLODAPv2.2016b_MappedClimatologies/GLODAPv2.2016b.salinity.nc") %>% 
+  hyper_tibble() %>% 
+  dplyr::select(lon, lat, depth_surface, salinity)
+GLODAP_temp_grid <- tidync("~/pCloudDrive/Data/GLODAPv2.2016b_MappedClimatologies/GLODAPv2.2016b.temperature.nc") %>% 
+  hyper_tibble() %>% 
+  dplyr::select(lon, lat, depth_surface, temperature)
 
-# Request form Jean-Pierre Gattuso for Arctic pCO2
+# Join depth data and correct longitudes to -180 to 180
+GLODAP_carb_chem_grid <- GLODAP_TCO2_grid %>% 
+  full_join(GLODAP_TAlk_grid, by = c("lon", "lat", "depth_surface")) %>% 
+  full_join(GLODAP_sal_grid, by = c("lon", "lat", "depth_surface")) %>% 
+  full_join(GLODAP_temp_grid, by = c("lon", "lat", "depth_surface")) %>% 
+  mutate(lon = ifelse(lon > 180, lon-360, lon)) %>% 
+  left_join(GLODAP_depth, by = c("depth_surface")) %>% 
+  dplyr::select(lon, lat, depth, TCO2, TAlk, salinity, temperature)
+
+# Request form Jean-Pierre Gattuso for Arctic carbonate chemistry layers
 # 200 metres and deeper, gridded data
 # 200 metres and shallower, station data (bottle data)
-Arctic_TCO2 <- GLODAP_Arctic_bottle_mean %>% 
+GLODAP_Arctic_carb_chem <- GLODAP_Arctic_bottle_mean %>% 
   filter(depth <= 200) %>% 
-  rbind(., filter(GLODAP_TCO2_grid, depth >= 200)) %>% 
+  rbind(., filter(GLODAP_carb_chem_grid, depth >= 200)) %>% 
   filter(lon >= min(GLODAP_Arctic_bottle_mean$lon),
          lon <= max(GLODAP_Arctic_bottle_mean$lon),
          lat >= min(GLODAP_Arctic_bottle_mean$lat),
          lat <= max(GLODAP_Arctic_bottle_mean$lat))
-write_csv(Arctic_TCO2, "data/Arctic_TCO2.csv")
+write_csv(GLODAP_Arctic_carb_chem, "data/GLODAP_Arctic_carb_chem.csv")
 
