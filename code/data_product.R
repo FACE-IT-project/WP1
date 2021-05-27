00479555df15c3b2935ba46bca688476fae38b55
Robert <robert.schlegel@imev-mer.fr>
2021-05-27 16:29:29 +0200

    First full version of kong PG product

--- code/data_product.R
+++ code/data_product.R
@@ -35,17 +35,20 @@ pg_quick_filter <- function(file_name, bbox){
 }
 
 # Function for melting columns related to a specific driver
-pg_var_melt <- function(pg_clean, key_words, var_name){
+pg_var_melt <- function(pg_clean, key_words, var_word){
   # Message of which columns were melted
-  print(colnames(dplyr::select(pg_clean, contains(key_words))))
+  sub_cols <- colnames(dplyr::select(pg_clean, contains(key_words)))
+  sub_cols <- sub_cols[!sub_cols %in% c("URL", "citation", "lon", "lat", "date", "depth")]
+  print(sub_cols)
   
   # Subset and melt data.frame
   pg_melt <- pg_clean %>% 
-    dplyr::select("URL", "citation", "lon", "lat", "date", "depth", contains(key_words)) %>% 
-    pivot_longer(cols = colnames(dplyr::select(pg_clean, contains(key_words))), 
-                 names_to = paste0(var_name,"_var"), values_to = var_name) %>% 
-    filter(!is.na((!!as.name(var_name)))) %>% 
-    distinct()
+    dplyr::select("URL", "citation", "lon", "lat", "date", "depth", sub_cols) %>% 
+    pivot_longer(cols = all_of(sub_cols), names_to = paste0("var_name"), values_to = "value") %>% 
+    mutate(var_type = var_word) %>% 
+    filter(!is.na(value)) %>%
+    distinct() %>% 
+    dplyr::select(URL:depth, var_type, var_name, value)
 }
 
 
@@ -84,34 +87,18 @@ pg_kong_clean <- pg_kong_sub %>%
                           date == "2009-07" ~ "2009-07-01",
                           date == "2003-06" ~ "2003-06-01",
                           TRUE ~ date),
-         date = gsub("T.*", "", date),
-         date = as.Date(date)) %>%
+         date = as.Date(gsub("T.*", "", date))) %>%
   # Manage depth column
   mutate(depth = case_when(!is.na(`Press [dbar] (Pressure sensor, Digiquartz)`) ~ as.numeric(`Press [dbar] (Pressure sensor, Digiquartz)`),
                            !is.na(`Press [dbar]`) ~ as.numeric(`Press [dbar]`),
                            !is.na(`Depth water [m]`) ~ as.numeric(`Depth water [m]`),
                            !is.na(`Depth [m]`) ~ as.numeric(`Depth [m]`))) %>% 
   # Remove unwanted columns
-  dplyr::select(-"ID", -"Station", - "parent_doi",
-                -"Longitude 2", -"Latitude 2",
+  dplyr::select(-"Longitude 2", -"Latitude 2",
                 -"Coverage", -"Date/time start", -"Date/time end",
                 -"Elevation [m]", -"Elevation [m a.s.l.]", -"Depth water [m]", -"Depth [m]", -"Depth top [m]",
                 -"Depth bot [m]", -"Press [dbar]", -"Press [dbar] (Pressure sensor, Digiquartz)",
-                -contains(c("Lu_", "Ed_", "Es_"))
-                # -"Visib [m]", -"Distance [m]", -"Ratio", -"C/N", -"C/Chl a",
-                # -contains(c("Lu_", "Ed_", "Es_", "File ", "Polygon", "Name", "Event", "URL ", "TZ ",
-                #             "topography", "floe", "Cloud ", "Bact", "Station", "Reference", "HNA", "LNA",
-                #             "Ophiopluteus", "Amphipoda", "Cyphonautes", "Tintinnopsis lata", "Sigma-theta",
-                #             "Lamellibranchiata", "Polychaeta", "Coelenterata", "Collection", "Fluores",
-                #             "Std dev", " biom ", "Replicate", "indet", "juveniles", "Length", "Density",
-                #             "Locality", "Local time", "Type", "PFDoDA", "Domain", "Course", "TDN",
-                #             "Basis", "Position", "Chrysophyta", "Sampling date", "TDP", "instrument depth",
-                #             "Corallinales ", "Serpulidae ", "Sample label", "Taxa", "Harpacticoida",
-                #             "Meiofauna", " biom ", "photo ", "Area", "nation", "Ord ", "ID ", "NOBS",
-                #             "[NO3]- + [NO2]", "AT [µmol/kg]", "CSC", "CO3", "fCO2",
-                #             "A. ", "B. ", "C. ", "D. ", "E. ", "F. ", "G. ", "H. ", "I. ", "J. ", "K. ", "L. ", "M. ",
-                #             "N. ", "O. ", "P. ", "Q. ", "R. ", "S. ", "T. ", "U. ", "V. ", "W. ", "X. ", "Y. ", "Z. "))
-                ) %>%
+                -contains(c("Lu_", "Ed_", "Es_"))) %>%
   # Finish up
   dplyr::select(URL, citation, lon, lat, date, depth, everything()) %>% 
   mutate_at(c(6:length(.)), as.numeric) %>% 
@@ -119,39 +106,52 @@ pg_kong_clean <- pg_kong_sub %>%
 colnames(pg_kong_clean)
 
 # Individual variable data.frames
+## Cryosphere
 pg_kong_cryo <- pg_var_melt(pg_kong_clean, c("ice", "snow"), "cryo") %>% 
-  filter(!cryo_var == "Tonicella sp. [#]")
-pg_kong_temp <- pg_var_melt(pg_kong_clean, c("°C"), "temp")
+  filter(!var_name == "Tonicella sp. [#]")
+## Physical
+pg_kong_temp <- pg_var_melt(pg_kong_clean, c("°C", "temp"), "temp") %>% 
+  filter(!var_name %in% c("DOC [µmol/l] (High temperature catalytic ox...)", 
+                          "TDN [µmol/l] (High temperature catalytic ox...)" ))
 pg_kong_sal <- pg_var_melt(pg_kong_clean, c("sal"), "sal")
 pg_kong_O2 <- pg_var_melt(pg_kong_clean, c("O2", "DO"), "O2") %>% 
-  filter(!O2_var %in% c("[NO2]- [µmol/l]", "[NO3]- + [NO2]- [µmol/l]", 
-                        "CO2 [µmol/kg]", "pCO2water_SST_wet [µatm]", "fCO2water_SST_wet [µatm]",
-                        "DOC [µmol/l] (High temperature catalytic ox...)", "DOC [µmol/l]"))
+  filter(!var_name %in% c("[NO2]- [µmol/l]", "[NO3]- + [NO2]- [µmol/l]", 
+                          "CO2 [µmol/kg]", "pCO2water_SST_wet [µatm]", "fCO2water_SST_wet [µatm]",
+                          "DOC [µmol/l] (High temperature catalytic ox...)", "DOC [µmol/l]"))
+pg_kong_current <- pg_var_melt(pg_kong_clean, c("u ", "v "), "current")
+pg_kong_turb <- pg_var_melt(pg_kong_clean, c("turbidity"), "turbidity")
+## Carbonate chemistry
 pg_kong_nutrient <- pg_var_melt(pg_kong_clean, c("NO3", "NO2", "NH3", "PO4", "Si"), "nutrient") %>% 
-  filter(!nutrient_var %in% c("[NO3]- + [NO2]- [µmol/l]", "Chl a Prasin [µg/l]", 
-                              "Sipunculidae biom wm [g/m**2]", "Floe size [code] (primary floe size)",
-                              "Sigma-theta [kg/m**3]", "Visib [m]", "Density [kg/m**3]",
-                              "Chl a Prasin [µg/l]", "File size [kByte]"))
+  filter(var_name %in% c("[NO3]- [µmol/l]", "[NO2]- [µmol/l]", "[PO4]3- [µmol/l]", "Si(OH)4 [µmol/l]"))
 pg_kong_dissolved <- pg_var_melt(pg_kong_clean, c("DIC", "DOC", "DON"), "dissolved") %>% 
-  filter(!dissolved_var %in% c("TDP [µmol/l] (Acidic molybdate solution)", "C. islandica [#]"))
+  filter(!var_name %in% c("TDP [µmol/l] (Acidic molybdate solution)", "C. islandica [#]"))
 pg_kong_CaCO3 <- pg_var_melt(pg_kong_clean, c("CaCO3", "omega", "arg", "cal"), "CaCO3") %>% 
-  filter(!CaCO3_var %in% c("Bact [#/ml] (FACSCalibur flow-cytometer (B...)", "HNA het [#/ml] (FACSCalibur flow-cytometer (B...)",
-                           "LNA [#/ml] (FACSCalibur flow-cytometer (B...)", "Sal (Sensor 1, corrected, Calculated)",
-                           "Sal (Sensor 2, corrected, Calculated)", "O2 [µmol/kg] (Sensor 1, not calibrated, der...)",
-                           "O2 [µmol/kg] (Sensor 2, not calibrated, der...)", "PAR [µmol quanta/m**2/s] (not calibrated)",
-                           "PAR [µmol quanta/m**2/s] (at surface (SPAR), not calibr...)", "Fluores [µg/l] (not calibrated, Fluorometer, ...)"))
+  filter(var_name %in% c("Omega Arg", "Omega Cal", "Arg [%] (High Performance Liquid Chrom...)"))
 pg_kong_CO2 <- pg_var_melt(pg_kong_clean, c("CO2"), "CO2") %>% 
-  filter(!CO2_var == "fCO2water_SST_wet [µatm]")
+  filter(!var_name == "fCO2water_SST_wet [µatm]")
+pg_kong_pH <- pg_var_melt(pg_kong_clean, c("pH", "AT"), "pH") %>% 
+  filter(var_name %in% c("pH", "AT [µmol/kg] (Potentiometric titration)", "AT [µmol/kg]"))
+# pg_kong_TA <- pg_var_melt(pg_kong_clean, c("TA"), "TA") # No values
+pg_kong_PAR <- pg_var_melt(pg_kong_clean, c("PAR"), "PAR") %>% 
+  filter(!var_name %in% c("Ice conc [tenths] (primary ice partial concentra...)",
+                          "O2 [µmol/l] (Winkler titration (Parsons et...)",
+                          "O2 sat [%] (Winkler titration (Parsons et...)"))
+## Biology
+pg_kong_Chl <- pg_var_melt(pg_kong_clean, c("Chl"), "Chl")
+## Social
+
 
 # Check a file to ensure only correct variables remain
-unique(pg_kong_CO2[,7])
+unique(pg_kong_PAR[,8])
 
-# Stitch them together
-
-
+# Stack them together
+pg_kong_final <- rbind(pg_kong_cryo, pg_kong_temp, pg_kong_sal, pg_kong_O2, pg_kong_current, pg_kong_turb,
+                       pg_kong_nutrient, pg_kong_dissolved, pg_kong_CaCO3, pg_kong_CO2, pg_kong_pH, 
+                       pg_kong_PAR, pg_kong_Chl)
 
 # Check that all columns were used
-c(pg_kong_final$a) %in% colnames(pg_kong_clean)
+colnames(pg_kong_clean)[!colnames(pg_kong_clean) %in% unique(pg_kong_final$var_name)]
+
 
 
 # Bits of code used when untangling a site product
