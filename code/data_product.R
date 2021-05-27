c3aa95424a61fc093b578f2072c26e9f50b2bf8a
Robert <robert.schlegel@imev-mer.fr>
2021-05-27 15:11:31 +0200

    Eased up on pre-filtering before variable specific data.frames

--- code/data_product.R
+++ code/data_product.R
@@ -34,15 +34,25 @@ pg_quick_filter <- function(file_name, bbox){
   return(pg_res)
 }
 
+# Function for melting columns related to a specific driver
+pg_var_melt <- function(pg_clean, key_words, var_name){
+  # Message of which columns were melted
+  print(colnames(dplyr::select(pg_clean, contains(key_words))))
+  
+  # Subset and melt data.frame
+  pg_melt <- pg_clean %>% 
+    dplyr::select("URL", "citation", "lon", "lat", "date", "depth", contains(key_words)) %>% 
+    pivot_longer(cols = colnames(dplyr::select(pg_clean, contains(key_words))), 
+                 names_to = paste0(var_name,"_var"), values_to = var_name) %>% 
+    filter(!is.na((!!as.name(var_name)))) %>% 
+    distinct()
+}
+
 
 # European Arctic ---------------------------------------------------------
 
-# First load the large files together in one stack for use later on
-# NB: This is too large to load in one whack
-# And I don't think we can load them individually all in memory either...
-# system.time(
-# pg_EU_sub <- plyr::ldply(pg_EU_files, pg_quick_filter, .parallel = T, bbox = bbox_EU)
-# ) # xxx seconds
+# No products are currently planned to be made for the EU Arctic
+# Rather access the existing files directly: ~/pCloud/EU_Arctic/...
 
 
 # Kongsfjorden ------------------------------------------------------------
@@ -87,34 +97,64 @@ pg_kong_clean <- pg_kong_sub %>%
                 -"Coverage", -"Date/time start", -"Date/time end",
                 -"Elevation [m]", -"Elevation [m a.s.l.]", -"Depth water [m]", -"Depth [m]", -"Depth top [m]",
                 -"Depth bot [m]", -"Press [dbar]", -"Press [dbar] (Pressure sensor, Digiquartz)",
-                -"Visib [m]", -"Distance [m]", -"Ratio", -"C/N", -"C/Chl a",
-                -contains(c("Lu_", "Ed_", "Es_", "File ", "Polygon", "Name", "Event", "URL ", "TZ ",
-                            "topography", "floe", "Cloud ", "Bact", "Station", "Reference", "HNA", "LNA",
-                            "Ophiopluteus", "Amphipoda", "Cyphonautes", "Tintinnopsis lata", "Sigma-theta",
-                            "Lamellibranchiata", "Polychaeta", "Coelenterata", "Collection", "Fluores",
-                            "Std dev", " biom ", "Replicate", "indet", "juveniles", "Length", "Density",
-                            "Locality", "Local time", "Type", "PFDoDA", "Domain", "Course", "TDN",
-                            "Basis", "Position", "Chrysophyta", "Sampling date", "TDP", "instrument depth",
-                            "Corallinales ", "Serpulidae ", "Sample label", "Taxa", "Harpacticoida",
-                            "Meiofauna", " biom ", "photo ", "Area", "nation", "Ord ", "ID ", "NOBS",
-                            "[NO3]- + [NO2]", "AT [µmol/kg]", "CSC", "CO3", "fCO2",
-                            "A. ", "B. ", "C. ", "D. ", "E. ", "F. ", "G. ", "H. ", "I. ", "J. ", "K. ", "L. ", "M. ",
-                            "N. ", "O. ", "P. ", "Q. ", "R. ", "S. ", "T. ", "U. ", "V. ", "W. ", "X. ", "Y. ", "Z. "))) %>%
+                -contains(c("Lu_", "Ed_", "Es_"))
+                # -"Visib [m]", -"Distance [m]", -"Ratio", -"C/N", -"C/Chl a",
+                # -contains(c("Lu_", "Ed_", "Es_", "File ", "Polygon", "Name", "Event", "URL ", "TZ ",
+                #             "topography", "floe", "Cloud ", "Bact", "Station", "Reference", "HNA", "LNA",
+                #             "Ophiopluteus", "Amphipoda", "Cyphonautes", "Tintinnopsis lata", "Sigma-theta",
+                #             "Lamellibranchiata", "Polychaeta", "Coelenterata", "Collection", "Fluores",
+                #             "Std dev", " biom ", "Replicate", "indet", "juveniles", "Length", "Density",
+                #             "Locality", "Local time", "Type", "PFDoDA", "Domain", "Course", "TDN",
+                #             "Basis", "Position", "Chrysophyta", "Sampling date", "TDP", "instrument depth",
+                #             "Corallinales ", "Serpulidae ", "Sample label", "Taxa", "Harpacticoida",
+                #             "Meiofauna", " biom ", "photo ", "Area", "nation", "Ord ", "ID ", "NOBS",
+                #             "[NO3]- + [NO2]", "AT [µmol/kg]", "CSC", "CO3", "fCO2",
+                #             "A. ", "B. ", "C. ", "D. ", "E. ", "F. ", "G. ", "H. ", "I. ", "J. ", "K. ", "L. ", "M. ",
+                #             "N. ", "O. ", "P. ", "Q. ", "R. ", "S. ", "T. ", "U. ", "V. ", "W. ", "X. ", "Y. ", "Z. "))
+                ) %>%
+  # Finish up
   dplyr::select(URL, citation, lon, lat, date, depth, everything()) %>% 
   mutate_at(c(6:length(.)), as.numeric) %>% 
   janitor::remove_empty("cols")
+colnames(pg_kong_clean)
 
-# Create guide with no empty rows
-pg_kong_thin <- pg_kong_clean %>% 
-  select(-c(URL:depth)) %>% 
-  janitor::remove_empty("rows")
+# Individual variable data.frames
+pg_kong_cryo <- pg_var_melt(pg_kong_clean, c("ice", "snow"), "cryo") %>% 
+  filter(!cryo_var == "Tonicella sp. [#]")
+pg_kong_temp <- pg_var_melt(pg_kong_clean, c("°C"), "temp")
+pg_kong_sal <- pg_var_melt(pg_kong_clean, c("sal"), "sal")
+pg_kong_O2 <- pg_var_melt(pg_kong_clean, c("O2", "DO"), "O2") %>% 
+  filter(!O2_var %in% c("[NO2]- [µmol/l]", "[NO3]- + [NO2]- [µmol/l]", 
+                        "CO2 [µmol/kg]", "pCO2water_SST_wet [µatm]", "fCO2water_SST_wet [µatm]",
+                        "DOC [µmol/l] (High temperature catalytic ox...)", "DOC [µmol/l]"))
+pg_kong_nutrient <- pg_var_melt(pg_kong_clean, c("NO3", "NO2", "NH3", "PO4", "Si"), "nutrient") %>% 
+  filter(!nutrient_var %in% c("[NO3]- + [NO2]- [µmol/l]", "Chl a Prasin [µg/l]", 
+                              "Sipunculidae biom wm [g/m**2]", "Floe size [code] (primary floe size)",
+                              "Sigma-theta [kg/m**3]", "Visib [m]", "Density [kg/m**3]",
+                              "Chl a Prasin [µg/l]", "File size [kByte]"))
+pg_kong_dissolved <- pg_var_melt(pg_kong_clean, c("DIC", "DOC", "DON"), "dissolved") %>% 
+  filter(!dissolved_var %in% c("TDP [µmol/l] (Acidic molybdate solution)", "C. islandica [#]"))
+pg_kong_CaCO3 <- pg_var_melt(pg_kong_clean, c("CaCO3", "omega", "arg", "cal"), "CaCO3") %>% 
+  filter(!CaCO3_var %in% c("Bact [#/ml] (FACSCalibur flow-cytometer (B...)", "HNA het [#/ml] (FACSCalibur flow-cytometer (B...)",
+                           "LNA [#/ml] (FACSCalibur flow-cytometer (B...)", "Sal (Sensor 1, corrected, Calculated)",
+                           "Sal (Sensor 2, corrected, Calculated)", "O2 [µmol/kg] (Sensor 1, not calibrated, der...)",
+                           "O2 [µmol/kg] (Sensor 2, not calibrated, der...)", "PAR [µmol quanta/m**2/s] (not calibrated)",
+                           "PAR [µmol quanta/m**2/s] (at surface (SPAR), not calibr...)", "Fluores [µg/l] (not calibrated, Fluorometer, ...)"))
+pg_kong_CO2 <- pg_var_melt(pg_kong_clean, c("CO2"), "CO2") %>% 
+  filter(!CO2_var == "fCO2water_SST_wet [µatm]")
 
-# Filter out empty rows
-suppressMessages( # Don't want column name joining message
-pg_kong_clean <- pg_kong_clean %>% 
-  right_join(pg_kong_thin)
-)
+# Check a file to ensure only correct variables remain
+unique(pg_kong_CO2[,7])
 
+# Stitch them together
+
+
+
+# Check that all columns were used
+c(pg_kong_final$a) %in% colnames(pg_kong_clean)
+
+
+# Bits of code used when untangling a site product
 colnames(pg_kong_clean)
 
 colnames(select(pg_kong_clean, contains("lon")))
@@ -122,32 +162,11 @@ colnames(select(pg_kong_clean, contains("lat")))
 colnames(select(pg_kong_clean, contains("date")))
 colnames(select(pg_kong_clean, contains(c("elev", "depth", "bathy", "press"))))
 
-pg_kong_clean[pg_kong_clean$date == "", ]
+pg_lon <- select(pg_kong_clean, "URL", "parent_doi", "citation", "lon", "Longitude 2", everything())
+pg_lat <- select(pg_kong_clean, "URL", "parent_doi", "citation", "lat", "Latitude 2", everything())
+pg_date <- select(pg_kong_clean, "URL", "parent_doi", "citation", contains("date"))
+pg_depth <- select(pg_kong_clean, "URL", "parent_doi", "citation", contains(c("elev", "depth", "bathy", "press")), everything())
 
-pg_kong_lon <- select(pg_kong_clean, "URL", "parent_doi", "citation", "lon", "Longitude 2", everything())
-pg_kong_lat <- select(pg_kong_clean, "URL", "parent_doi", "citation", "lat", "Latitude 2", everything())
-pg_kong_date <- select(pg_kong_clean, "URL", "parent_doi", "citation", contains("date"))
-pg_kong_depth <- select(pg_kong_clean, "URL", "parent_doi", "citation", contains(c("elev", "depth", "bathy", "press")), everything())
-
-# Melt common columns
-pg_kong_melt <- pg_kong_clean %>% 
-  # Depth values
-  pivot_longer(cols = colnames(dplyr::select(., contains(c("ice")))), 
-               names_to = "ice_name", values_to = "ice") %>% 
-  # Ice values
-  pivot_longer(cols = colnames(dplyr::select(., contains(c("ice")))), 
-               names_to = "ice_name", values_to = "ice") %>% 
-  # filter(!is.na(ice)) %>% 
-  # °C values
-  pivot_longer(cols = colnames(dplyr::select(., contains(c("°C")))), 
-               names_to = "temp_name", values_to = "temp")
-  
-
- #%>% 
-# Filter rows that are missing all values
-  # dplyr::select(-contains(c("Sample ID", "Comment", "pen depth", "105°C", "std dev", "TiO2", "parent", 
-                            # "feldspar", "Rock", "Pseudo", "DOY", "Cibici", "Device", "parva", "Amp", "sp.", "spp.", "Euphaus",
-                            # "Roughness", "rhaphid", "Cluster", "Trans", "residues", "falcon", "clausi", "Site"))) %>% 
 
 
 # Isfjorden ---------------------------------------------------------------
