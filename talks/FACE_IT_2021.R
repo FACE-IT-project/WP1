322fd258b1aa8e50a5daa790e0c4ec1ada8f1269
Robert <robert.schlegel@imev-mer.fr>
2021-09-15 16:09:28 +0000

    Created new folder to contain scripts and outputs for talks

---
+++ talks/FACE_IT_2021.R
@@ -0,0 +1,21 @@
+# talks/FACE_IT_2021.R
+# This script contains the code used for the analyses shown in 
+# the talk given for the FACE-IT 2021 annual meeting
+
+
+# Setup -------------------------------------------------------------------
+
+source("code/functions.R")
+
+
+
+# Data --------------------------------------------------------------------
+
+
+
+# Analyses ----------------------------------------------------------------
+
+# Show what the relationship has been between ship mileage and temperature or ice change
+# Then show what the different model RCP projections are and what the future may hold
+# Also use these relationship projections for any sort of biomass
+
