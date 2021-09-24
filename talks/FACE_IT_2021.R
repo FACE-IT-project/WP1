e013751dc5ffbac7a5d64b499300993be1022a4b
Robert <robert.schlegel@imev-mer.fr>
2021-09-24 14:36:59 +0000

    More structure points

--- talks/FACE_IT_2021.R
+++ talks/FACE_IT_2021.R
@@ -2,43 +2,13 @@
 # This script contains the code used for the analyses shown in 
 # the talk given for the FACE-IT 2021 annual meeting
 
-# TODO: Two talks
-# One as a WP1 update for just the work package
-  # Answer the questions in the meeting agenda update
-  # Talk about issue with Inglefieldbukta bounding box
-  # What is the scientific justification of choosing a semi-open fjord and one that is so large
-  # Give Janne a heads up first
-# The other as the talk that has an abstract submitted
+# TODO:
 # Ask Janne about cruise ship arrival/presence data for Isfjorden
 # Ask someone in Kings Bay (Geir as a first point of contact) for cruise ships in Kongsfjorden
   # Ship size and number of tourists where possible
   # This could be tracked down by getting the ship names then doing a bit of digging
 # Question for Grete: Is the disappearance of sea ice promoting tourism in the two Svalbard fjords
 # What may the triad relationship be between sea ice, cruise ships, and plankton biomass
-
-
-# Talk 1:
-# Discuss the data issues
-# Add a talking point to the WP1 talk about effectively coordinating between other data managers
-## Who is responsible for what?
-## Who has other people working with data collection
-## What should be done about more complex data types like 
-## Explain that WP1 is of service to everybody, but we can't provide this service without replies
-# Discuss complex data types like the seabird database and the geospatial glacier fronts
-# Potentially finish with the region analysis of Kongsfjorden and show people the link to where all the site analyses are
-## Boxplots could be useful as well as dot/line plots
-## Ask about what to do with cryosphere/atmospheric land data
-
-
-# Talk 2:
-# Have a specific focus on social science data
-## This is important because little effort has been done for social science so fart
-## List the social science data that have been identified
-## Solicit new input from people in the crowd about other social data, including Greenland
-## What sort of non-social data do social scientists want
-## Get the number of inhabitants of Longyearbyen over time and relate that to nutrient concentration
-# Keep in mind that this analysis is just an example to stimulate the audience to think about real analyses
-# These then are to be used for the review paper
 
 
 # Setup -------------------------------------------------------------------
@@ -68,7 +38,3 @@ source("code/functions.R")
   # The obvious contributors are the site coordinators and who they think are specialists in certain aspects at there site
 # Think of a timeline for the review article
 
-# The data from Google sheet 99 is kongsfjorden/d_all.rds
-# Contact JP about any questions regarding the data
-# Also contact JP for the reference for the data
-  # There is a draft data paper at the moment but not ready yet
