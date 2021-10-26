377fb9a9528d3ac92db1ce419780f48bb4964498
robwschlegel <robwschlegel@gmail.com>
2021-10-26 19:30:25 +0200

    Rough draft of the SSC poster

---
+++ code/SSC_2021.R
@@ -0,0 +1,22 @@
+# code/SSC_2021.R
+# This script contains the code used for the analyses and figures in the SSC 2021 poster
+
+# Setup -------------------------------------------------------------------
+
+library(tidyverse)
+library(heatwaveR)
+
+
+# Data --------------------------------------------------------------------
+
+
+# Analyses ----------------------------------------------------------------
+
+
+
+# Figures -----------------------------------------------------------------
+
+## Figure 1: Map and time series of Kongsfjorden
+
+## Figure 2: Bubble plot showing relationships of variables
+
