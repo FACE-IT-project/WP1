6f48b7dc7a89b197e7d7716f9ea379d44853bdf8
robwschlegel <robwschlegel@gmail.com>
2022-02-01 14:43:03 +0100

    Outline for ice cover and ship AIS analysis

---
+++ code/ice_cover_and_AIS.R
@@ -0,0 +1,40 @@
+# code# code/ice_cover_and_AIS.R
+# This script contains the code used to prep and process ice cover and ship AIS (presence) data
+# Currently this is only done for Isfjorden, but could be expanded in the future
+
+
+# Setup -------------------------------------------------------------------
+
+# Libraries used
+library(tidyverse)
+
+# Ice data
+
+# Ship AIS data
+
+
+# Process ice data --------------------------------------------------------
+
+# Get count of open water pixels in the fjord
+
+# Find proportion per month per year that has ice
+
+# Plot the monthly proportions over time
+
+# Calculate annual and monthly trends
+
+
+# Process AIS data --------------------------------------------------------
+
+# Get daily count of unique ships in the fjord
+
+# Get daily count of pixels for unique ships
+
+# Get daily count of time in fjord for unique ships
+
+# Get daily distance of unique ships
+
+# Plot the monthly values for ships in the fjord
+
+# Calculate annual and monthly trends
+
