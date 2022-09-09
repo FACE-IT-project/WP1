ad06b1b9346bc2168f2ee91bfe250705c60c5e54
Robert <robert.schlegel@imev-mer.fr>
2022-09-09 14:06:09 +0000

    Touch-ups for panel A) of the LOV poster figure

--- code/LOV_poster.R
+++ code/LOV_poster.R
@@ -53,32 +53,56 @@ cat_colours <- c("violet", "skyblue", "#F6EA7C", "#A2ED84", "#F48080")
 trend_colours <- c("blue", "purple", "red")
 V(net)$cat_colour <- cat_colours[V(net)$cat_num]
 V(net)$trend_colour <- trend_colours[V(net)$trend_num]
+V(net)$name
+
+# Dataframe to manually place individual labels
+net_label_coords <- data.frame(label = nodes$driver[1:14],
+                               x = c(1.02, 0.96, 0.68, # Cryosphere
+                                     0.23, -0.22, -0.63, # Physics
+                                     -0.9, -1.0, # Chemistry
+                                     -0.95, -0.63, -0.225, # Biology
+                                     0.225, 0.63, 0.91), # Social
+                               y = c(0.088, 0.523, 0.87, # Cryosphere
+                                     1.062, 1.062, 0.872, # Physics
+                                     0.52, 0.09, # Chemistry
+                                     -0.523, -0.87, -1.063, # Biology
+                                     -1.063, -0.871, -0.523)) # Social
 
 # Circle network
 panel_a <- ggraph(net, layout = "circle") +
-  geom_edge_fan(aes(colour = to_sign), width = 2, alpha = 0.8,
+  geom_edge_fan(aes(colour = to_sign), width = 2, alpha = 0.8, show.legend = F,
                  arrow = arrow(length = unit(0.025, "npc"), type = "closed")) +
   geom_node_point(aes(fill = category, colour = trend),
                   shape = 21, size = 16, stroke = 3) +
-  geom_node_label(aes(label = driver), size = 4, color = "black", alpha = 0.8,
-                  label.padding = unit(0.3, "lines"), label.size = unit(0.7, "lines")) +
+  # geom_node_label(aes(label = driver), size = 4, color = "black", alpha = 0.8,
+  #                 label.padding = unit(0.3, "lines"), label.size = unit(0.7, "lines")) +
+  geom_label(data = net_label_coords, aes(label = label, x = x, y = y), size = 6, color = "black",
+             label.padding = unit(0.3, "lines"), label.size = unit(0.7, "lines")) +
   scale_edge_colour_manual("Trend/\nImpact", 
                            breaks = c("increase", "decrease", "complex"),
-                           values = c("purple", "blue", "red")) +
-  scale_colour_manual("Trend/\nImpact",
+                           values = c("purple", "blue", "red"),
+                           guide_legend(order = 2, override.aes = list(shape = 15, size = 16))) +
+  scale_colour_manual("Trend or impact\nfor driver",
                       breaks = c("increase", "decrease", "complex"),
                       values = c("red", "blue", "purple")) +
   scale_fill_manual("Category", 
                     breaks = c("cryosphere", "physics", "chemistry", "biology", "social"),
                     values = c("mintcream", "skyblue", "#F6EA7C", "#A2ED84", "#F48080")) +
-  # labs(title = "Relationships of key drivers in Arctic fjords") +
+  scale_x_continuous(limits = c(-1.2, 1.2)) +
+  guides(colour = guide_legend(order = 1, label.position = "left", override.aes = list(shape = 15, size = 16)),
+  # guides(colour = guide_legend(order = 1),
+         # edge_colour = guide_legend(order = 2, override.aes = list(edge_colour_shape = 15)),
+         fill = guide_legend(order = 3)) +
   theme_void() +
   theme(plot.background = element_rect(fill = "grey90", colour = NA),
-        legend.title = element_text(size = 13),
-        legend.text = element_text(size = 11),
-        plot.margin = margin(t = 0, r = 20, b = 0, l = 10, unit = "pt"))
-panel_a
-ggsave("~/Desktop/panel_a.png", width = 12, height = 9)
+        # plot.margin = margin(t = 20, r = 170, b = 20, l = 20, unit = "pt")
+        # legend.position = c(1.09, 0.27),
+        legend.title = element_text(size = 16),
+        legend.text = element_text(size = 15),
+        legend.background = element_rect(fill = "white", colour = "black"),
+        legend.margin = margin(t = 10, r = 10, b = 10, l = 10, unit = "pt"))
+# panel_a
+ggsave("~/Desktop/panel_a.png", width = 16, height = 12)
 
 
 # Panel B -----------------------------------------------------------------
@@ -87,19 +111,8 @@ ggsave("~/Desktop/panel_a.png", width = 12, height = 9)
 # Colour by category with sub bars for each driver
 # Text labels showing total number of datasets with data for that driver
 
-# Load meta data
-load("data/analyses/all_meta.RData")
-
 # Load all clean data
 load("data/analyses/clean_all.RData")
-
-# Filter out remote products
-all_meta_insitu <- filter(all_meta, type == "in situ") %>% 
-  mutate(month = lubridate::month(date), 
-         site = factor(site, levels = c("kong", "is", "stor", "young", "disko", "nuup", "por"),
-                       labels = c("Kongsfjorden", "Isfjorden", "Storfjorden", 
-                                  "Young Sound", "Disko Bay", "Nuup Kangelrua",
-                                  "Porsangerfjorden")))
 
 # Get metadata for driver data coverage and total datasets
 data_meta <- clean_all %>% 
@@ -177,7 +190,7 @@ panel_b <- ggplot(panel_b_data) +
         axis.ticks.y = element_blank(),
         axis.text = element_text(size = 10))
 panel_b
-ggsave("~/Desktop/panel_b.png", width = 12, height = 9)
+ggsave("~/Desktop/panel_b.png", width = 16, height = 12)
 
 
 # Final -------------------------------------------------------------------
@@ -186,7 +199,7 @@ ggsave("~/Desktop/panel_b.png", width = 12, height = 9)
 fig_1 <- ggpubr::ggarrange(panel_a, panel_b, ncol = 2, labels = c("A)", "B)"))
 
 # Save
-ggsave("figures/LOV_fig_1.png", width = 24, height = 9)
+ggsave("figures/LOV_fig_1.png", width = 30, height = 12)
 
 # Text
 # Figure XX: The two primary objectives of WP1: A) A) understanding the relationships between the key drivers of change 
