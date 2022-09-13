d0cf0c19cfde9aeaebc3cd50a4b0c7c11d46b290
Robert <robert.schlegel@imev-mer.fr>
2022-09-13 13:29:17 +0200

    Commit before changes to LOV poster figures

--- code/LOV_poster.R
+++ code/LOV_poster.R
@@ -45,6 +45,95 @@ edges <- read_csv("data/figures/rp_fig_2_edges.csv") %>%
 
 # Panel A -----------------------------------------------------------------
 
+# Horizontal barplots showing time over which data are available
+# Colour by category with sub bars for each driver
+# Text labels showing total number of datasets with data for that driver
+
+# Load all clean data
+load("data/analyses/clean_all.RData")
+
+# Get metadata for driver data coverage and total datasets
+data_meta <- clean_all %>% 
+  filter(type == "in situ",
+         var_group %in% c("Ice vars", "Glacier vars", "river", 
+                          "Sea temp", "Salinity", "PAR",          
+                          "pCO2", "Nutrients",
+                          "Chla", "Biomass", "Species",
+                          "Tourism", "Shipping" )) %>% 
+  group_by(citation, var_group) %>% 
+  summarise(start_date = min(date, na.rm = T), end_date = max(date, na.rm = T), 
+            data_points = n(), .groups = "drop") %>% 
+  group_by(var_group) %>% 
+  summarise(min_date = min(start_date, na.rm = T), max_date = max(end_date, na.rm = T), 
+            data_sets = n(), data_points = sum(data_points, na.rm = T), .groups = "drop") %>% 
+  rbind(data.frame(var_group = "Governance", min_date = NA, max_date = NA, data_sets = 1, data_points = 1))
+
+# Clean up for plotting
+panel_a_data <- data_meta %>% 
+  mutate(driver = case_when(var_group == "Biomass" ~ "biomass",
+                            var_group == "Chla" ~ "primary production",
+                            var_group == "Glacier vars" ~ "glacier mass balance",
+                            var_group == "Ice vars" ~ "sea ice",
+                            var_group == "Nutrients" ~ "nutrients",
+                            var_group == "PAR" ~ "light",
+                            var_group == "pCO2" ~ "carbonate system",
+                            var_group == "river" ~ "glacial + river discharge",
+                            var_group == "Salinity" ~ "salinity",
+                            var_group == "Sea temp" ~ "seawater temperature",
+                            var_group == "Shipping" ~ "fisheries",
+                            var_group == "Species" ~ "species richness",
+                            var_group == "Tourism" ~ "tourism",
+                            var_group == "Governance" ~ "governance"),
+         category = case_when(driver %in% c("sea ice", "glacier mass balance", "glacial + river discharge") ~ "cryosphere",
+                              driver %in% c("seawater temperature", "salinity", "light") ~ "physics",
+                              driver %in% c("carbonate system", "nutrients") ~ "chemistry",
+                              driver %in% c("primary production", "biomass", "species richness") ~ "biology",
+                              driver %in% c("governance", "tourism", "fisheries") ~ "social"),
+         driver = factor(driver, levels = c("sea ice", "glacier mass balance", "glacial + river discharge",
+                                            "seawater temperature", "salinity", "light",
+                                            "carbonate system", "nutrients",
+                                            "primary production", "biomass", "species richness",
+                                            "governance", "tourism", "fisheries")))#,
+         # data_points = scales::comma(data_points))
+
+# Plot the data
+panel_a <- ggplot(panel_a_data, aes(y = driver, x = log10(data_points))) +
+  geom_segment(aes(y = driver, yend = driver, x = 0, xend = log10(data_points)),
+               colour = "black", size =  11) +
+  geom_point(aes(x = min_date, y = driver, fill = category), 
+             shape = 21, size = 14, stroke = 1.2, show.legend = F) +
+  geom_point(aes(x = max_date, y = driver, fill = category), 
+             shape = 21, size = 14, stroke = 1.2, show.legend = F) +
+  geom_segment(aes(x = min_date, xend = max_date, y = driver, yend = driver, colour = category), 
+               size = 9, show.legend = F) +
+  geom_label(aes(label = paste0(driver,"\nDatasets: ",data_sets,"\nData points: ",data_points),
+                 x = as.Date("2024-12-31"), y = driver, hjust = "right"), alpha = 1, size = 6,
+             label.padding = unit(0.3, "lines"), label.size = unit(0.7, "lines")) +
+  scale_y_discrete(limits = rev, position = "right") +
+  scale_x_date(limits = c(as.Date("1871-01-01"), as.Date("2070-12-31")),
+               date_labels = "%Y",
+               breaks = seq(as.Date("1877-01-01"), as.Date("2021-01-01"), length = 7),
+               expand = c(0, 0)) +
+  scale_colour_manual("Category", aesthetics = c("colour", "fill"),
+                      breaks = c("cryosphere", "physics", "chemistry", "biology", "social"),
+                      values = c("mintcream", "skyblue", "#F6EA7C", "#A2ED84", "#F48080")) +
+  labs(x = NULL, y = NULL) +
+  theme(plot.background = element_rect(fill = "grey90", colour = NA),
+        axis.text.y = element_blank(),
+        plot.margin = margin(t = 10, r = -20, b = 10, l = 20, unit = "pt"),
+        panel.grid = element_line(colour = "black"),
+        panel.grid.major.y = element_line(colour = NA),
+        panel.grid.minor.x = element_line(colour = NA),
+        panel.border = element_rect(colour = NA, fill = NA),
+        panel.background = element_rect(fill = NA, colour = NA),
+        axis.ticks.y = element_blank(),
+        axis.text = element_text(size = 15))
+# panel_a
+ggsave("~/Desktop/panel_a.png", panel_b, width = 16, height = 14)
+
+
+# Panel B -----------------------------------------------------------------
+
 # Create network
 net <- graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
 
@@ -69,7 +158,7 @@ net_label_coords <- data.frame(label = nodes$driver[1:14],
                                      -1.063, -0.871, -0.523)) # Social
 
 # Circle network
-panel_a <- ggraph(net, layout = "circle") +
+panel_b <- ggraph(net, layout = "circle") +
   geom_edge_fan(aes(colour = to_sign), width = 2, alpha = 0.8, show.legend = F,
                  arrow = arrow(length = unit(0.025, "npc"), type = "closed")) +
   geom_node_point(aes(fill = category, colour = trend),
@@ -101,98 +190,8 @@ panel_a <- ggraph(net, layout = "circle") +
         legend.text = element_text(size = 15),
         legend.background = element_rect(fill = "white", colour = "black"),
         legend.margin = margin(t = 10, r = 20, b = 10, l = 20, unit = "pt"))
-# panel_a
-ggsave("~/Desktop/panel_a.png", panel_a, width = 16, height = 14)
-
-
-# Panel B -----------------------------------------------------------------
-
-# Horizontal barplots showing time over which data are available
-# Colour by category with sub bars for each driver
-# Text labels showing total number of datasets with data for that driver
-
-# Load all clean data
-load("data/analyses/clean_all.RData")
-
-# Get metadata for driver data coverage and total datasets
-data_meta <- clean_all %>% 
-  filter(type == "in situ",
-         var_group %in% c("Ice vars", "Glacier vars", "river", 
-                          "Sea temp", "Salinity", "PAR",          
-                          "pCO2", "Nutrients",
-                          "Chla", "Biomass", "Species",
-                          "Tourism", "Shipping" )) %>% 
-  group_by(citation, var_group) %>% 
-  summarise(start_date = min(date, na.rm = T), end_date = max(date, na.rm = T), 
-            data_points = n(), .groups = "drop") %>% 
-  group_by(var_group) %>% 
-  summarise(min_date = min(start_date, na.rm = T), max_date = max(end_date, na.rm = T), 
-            data_sets = n(), data_points = sum(data_points, na.rm = T), .groups = "drop")
-
-# Clean up for plotting
-panel_b_data <- data_meta %>% 
-  mutate(driver = case_when(var_group == "Biomass" ~ "biomass",
-                            var_group == "Chla" ~ "primary production",
-                            var_group == "Glacier vars" ~ "glacier mass balance",
-                            var_group == "Ice vars" ~ "sea ice",
-                            var_group == "Nutrients" ~ "nutrients",
-                            var_group == "PAR" ~ "light",
-                            var_group == "pCO2" ~ "carbonate system",
-                            var_group == "river" ~ "glacial + river discharge",
-                            var_group == "Salinity" ~ "salinity",
-                            var_group == "Sea temp" ~ "seawater temperature",
-                            var_group == "Shipping" ~ "fisheries",
-                            var_group == "Species" ~ "species richness",
-                            var_group == "Tourism" ~ "tourism"),
-         category = case_when(driver %in% c("sea ice", "glacier mass balance", "glacial + river discharge") ~ "cryosphere",
-                              driver %in% c("seawater temperature", "salinity", "light") ~ "physics",
-                              driver %in% c("carbonate system", "nutrients") ~ "chemistry",
-                              driver %in% c("primary production", "biomass", "species richness") ~ "biology",
-                              driver %in% c("tourism", "fisheries") ~ "social"),
-         driver = factor(driver, levels = c("sea ice", "glacier mass balance", "glacial + river discharge",
-                                            "seawater temperature", "salinity", "light",
-                                            "carbonate system", "nutrients",
-                                            "primary production", "biomass", "species richness",
-                                            "tourism", "fisheries")),
-         data_points = scales::comma(data_points))
-
-# Plot the data
-panel_b <- ggplot(panel_b_data) +
-  geom_segment(aes(x = min_date, xend = max_date, y = driver, yend = driver),
-               colour = "black", size =  11) +
-  geom_point(aes(x = min_date, y = driver, fill = category), 
-             shape = 21, size = 14, stroke = 1.2, show.legend = F) +
-  geom_point(aes(x = max_date, y = driver, fill = category), 
-             shape = 21, size = 14, stroke = 1.2, show.legend = F) +
-  geom_segment(aes(x = min_date, xend = max_date, y = driver, yend = driver, colour = category), 
-               size = 9, show.legend = F) +
-  # geom_text(aes(label = driver, x = ))
-  geom_label(aes(label = paste0(driver,"\nDatasets: ",data_sets,"\nData points: ",data_points),
-                 x = as.Date("2024-12-31"), y = driver, hjust = "right"), alpha = 1, size = 6,
-             label.padding = unit(0.3, "lines"), label.size = unit(0.7, "lines")) +
-  scale_y_discrete(limits = rev, position = "right") +
-  scale_x_date(limits = c(as.Date("1871-01-01"), as.Date("2070-12-31")),
-               # date_breaks = "20 years",
-               date_labels = "%Y",
-               breaks = seq(as.Date("1877-01-01"), as.Date("2021-01-01"), length = 7),
-               expand = c(0, 0)) +
-  scale_colour_manual("Category", aesthetics = c("colour", "fill"),
-                      breaks = c("cryosphere", "physics", "chemistry", "biology", "social"),
-                      values = c("mintcream", "skyblue", "#F6EA7C", "#A2ED84", "#F48080")) +
-  labs(x = NULL, y = NULL) +
-  theme(plot.background = element_rect(fill = "grey90", colour = NA),
-        axis.text.y = element_blank(), 
-        # axis.line = element_line(colour = "black"), 
-        plot.margin = margin(t = 10, r = -20, b = 10, l = 20, unit = "pt"),
-        panel.grid = element_line(colour = "black"),
-        panel.grid.major.y = element_line(colour = NA),
-        panel.grid.minor.x = element_line(colour = NA),
-        panel.border = element_rect(colour = NA, fill = NA),
-        panel.background = element_rect(fill = NA, colour = NA),
-        axis.ticks.y = element_blank(),
-        axis.text = element_text(size = 15))
 # panel_b
-ggsave("~/Desktop/panel_b.png", panel_b, width = 16, height = 14)
+ggsave("~/Desktop/panel_a.png", panel_a, width = 16, height = 14)
 
 
 # Final -------------------------------------------------------------------
