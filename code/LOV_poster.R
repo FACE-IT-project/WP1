99ea6d0fec0cf074b8cff5f2e5a95639d2665951
Robert <robert.schlegel@imev-mer.fr>
2022-09-13 15:18:42 +0000

    Updated LOV figure

--- code/LOV_poster.R
+++ code/LOV_poster.R
@@ -46,9 +46,8 @@ edges <- read_csv("data/figures/rp_fig_2_edges.csv") %>%
 
 # Panel A -----------------------------------------------------------------
 
-# Horizontal barplots showing time over which data are available
+# Horizontal barplots showing the total data points available
 # Colour by category with sub bars for each driver
-# Text labels showing total number of datasets with data for that driver
 
 # Load all clean data
 load("data/analyses/clean_all.RData")
@@ -113,19 +112,18 @@ panel_a <- ggplot(panel_a_data, aes(x = data_points_log10, y = driver_num)) +
                colour = "black", size =  13) +
   geom_point(data = panel_a_data[-14,], 
              aes(x = data_points_log10, y = driver_num, fill = category), 
-             shape = 21, size = 14, stroke = 0.8, show.legend = F) +
-  # annotate("label", x = 5.2, y = "glacier mass balance", label = "Cryosphere", size = 10, fontface = "bold", angle = 310) +
-  # annotate("text", x = 5, y = "glacier mass balance", label = "{", size = 30, angle = 220, xnudge = 0.1) +
-  # geom_point(aes(x = max_date, y = driver, fill = category), 
-  #            shape = 21, size = 14, stroke = 1.2, show.legend = F) +
+             shape = 21, size = 16, stroke = 0.8, show.legend = F) +
   geom_segment(data = panel_a_data[-14,],
                aes(x = 0, xend = data_points_log10, y = driver_num, yend = driver_num, colour = category), 
                size = 12, show.legend = F) +
   geom_text(aes(x = 0.1, label = driver), hjust = "left", size = 7) +
-  geom_richtext(data = panel_a_labels, aes(x = -0.15, y = y_num, label = category, fill = category), angle = 90,
-                size = 9, label.padding = unit(0.3, "lines"), label.size = unit(0.7, "lines"), show.legend = F) +
+  geom_richtext(data = panel_a_labels, hjust = 0.5,
+                aes(x = -0.2, y = y_num, label = category, fill = category),
+                angle = 90, size = 9, show.legend = F, label.r = unit(1, "lines"),
+                label.margin = unit(c(0.5, 0.5, 0.5, 0.5), "lines"),
+                label.padding = unit(c(0.5, 1.5, 0.5, 0.5), "lines")) + # Note that ggarrange messes this up
   scale_y_reverse() +
-  scale_x_continuous(limits = c(-0.3, 6.8),
+  scale_x_continuous(limits = c(-0.4, 6.8),
                      breaks = seq(1, 6, 1),
                      labels = c("10", "100", "1,000", "10,000", "100,000", "1,000,000"),
                      expand = c(0, 0)) +
@@ -135,7 +133,7 @@ panel_a <- ggplot(panel_a_data, aes(x = data_points_log10, y = driver_num)) +
   labs(x = "Data points [log10]", y = NULL) +
   theme(plot.background = element_rect(fill = "grey90", colour = NA),
         # axis.text.y = element_blank(),
-        plot.margin = margin(t = 10, r = 0, b = 10, l = 0, unit = "pt"),
+        plot.margin = margin(t = 30, r = 40, b = 10, l = 0, unit = "pt"),
         panel.grid = element_line(colour = "black"),
         panel.grid.major.y = element_line(colour = NA),
         panel.grid.minor.y = element_line(colour = NA),
@@ -152,21 +150,16 @@ panel_a <- ggplot(panel_a_data, aes(x = data_points_log10, y = driver_num)) +
 ggsave("~/Desktop/panel_a.png", panel_a, width = 14, height = 12)
 
 
+# Panel B -----------------------------------------------------------------
+
 
 # Create network
 net <- graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
 
-# Generate colors based on category:
-# cat_colours <- c("violet", "skyblue", "#F6EA7C", "#A2ED84", "#F48080")
-# trend_colours <- c("blue", "purple", "red")
-# V(net)$cat_colour <- cat_colours[V(net)$cat_num]
-# V(net)$trend_colour <- trend_colours[V(net)$trend_num]
-# V(net)$name
-
 # Dataframe to manually place individual labels
 net_label_coords <- data.frame(label = nodes$driver[1:14],
                                category = nodes$category[1:14],
-                               x = c(1.07, 1.02, 0.83, # Cryosphere
+                               x = c(1.07, 0.98, 0.85, # Cryosphere
                                      0.23, -0.22, -0.63, # Physics
                                      -0.92, -1.0, # Chemistry
                                      -0.95, -0.63, -0.225, # Biology
@@ -183,55 +176,49 @@ panel_b <- ggraph(net, layout = "circle") +
                  arrow = arrow(length = unit(0.025, "npc"), type = "closed")) +
   geom_node_point(aes(fill = category, colour = trend),
                   shape = 21, size = 16, stroke = 3) +
-  # geom_node_label(aes(label = driver), size = 4, color = "black", alpha = 0.8,
-  #                 label.padding = unit(0.3, "lines"), label.size = unit(0.7, "lines")) +
   geom_label(data = net_label_coords, aes(label = label, x = x, y = y, fill = category), 
-             size = 6, color = "black", show.legend = F,
-             label.padding = unit(0.3, "lines"), label.size = unit(0.7, "lines")) +
+             size = 7, color = "black", show.legend = F,
+             label.padding = unit(0.4, "lines"), label.size = unit(0.7, "lines")) +
   scale_edge_colour_manual("Trend/\nImpact", 
                            breaks = c("increase", "decrease", "complex"),
-                           values = c("purple", "blue", "red"),
-                           guide_legend(order = 2, override.aes = list(shape = 15, size = 16))) +
-  scale_colour_manual("Trend or impact\nfor driver",
+                           values = c("purple", "blue", "red")) +
+  scale_colour_manual("Driver trends\nand impacts",
                       breaks = c("increase", "decrease", "complex"),
-                      labels = c("increse", "decreae", "both"),
+                      labels = c("increse", "decrease", "both"),
                       values = c("red", "blue", "purple")) +
   scale_fill_manual("Category", 
                     breaks = c("cryosphere", "physics", "chemistry", "biology", "social"),
                     values = c("mintcream", "skyblue", "#F6EA7C", "#A2ED84", "#F48080")) +
-  scale_x_continuous(limits = c(-1.2, 1.2)) +
+  scale_x_reverse(limits = c(1.2, -1.2)) +
   guides(colour = guide_legend(order = 1, label.position = "right", override.aes = list(shape = 15, size = 16)),
-  # guides(colour = guide_legend(order = 1),
-         # edge_colour = guide_legend(order = 2, override.aes = list(edge_colour_shape = 15)),
-         # fill = guide_legend(order = 3, label.position = "left", override.aes = list(stroke = 2))) +
-        fill = "none") +
+         fill = "none") +
   theme_void() +
   theme(plot.background = element_rect(fill = "grey90", colour = NA),
-        plot.margin = margin(t = -10, r = 0, b = -10, l = -20, unit = "pt"),
-        legend.position = c(0.95, 0.15),
-        legend.title = element_text(size = 16),
-        legend.text = element_text(size = 15),
+        plot.margin = margin(t = -10, r = -10, b = -10, l = 0, unit = "pt"),
+        legend.position = c(0.908, 0.136),
+        legend.title = element_text(size = 20),
+        legend.text = element_text(size = 18),
         legend.background = element_rect(fill = "white", colour = "black"),
-        legend.margin = margin(t = 10, r = 20, b = 10, l = 20, unit = "pt"))
-panel_b
+        legend.margin = margin(t = 10, r = 15, b = 10, l = 10, unit = "pt"))
+# panel_b
 ggsave("~/Desktop/panel_b.png", panel_b, width = 14, height = 12)
 
 
 # Final -------------------------------------------------------------------
 
 # Combine
-fig_1 <- ggpubr::ggarrange(panel_a, panel_b, ncol = 2, widths = c(1, 1),
-                           labels = c("A)", "B)"), font.label = list(size = 22))
+fig_11 <- ggpubr::ggarrange(panel_a, panel_b, ncol = 2,
+                           labels = c("A)", "B)"), font.label = list(size = 24))
 
 # Save
-ggsave("figures/LOV_fig_1.png", fig_1, width = 32, height = 14)
+ggsave("figures/LOV_fig_1.png", fig_1, width = 28, height = 12)
 
 # Text
-# Figure XX: The two primary objectives of WP1: A) understanding the relationships between the key drivers of change 
-# in socio-ecological Arctic fjords systems and B) managing & analysing the corresponding data.
-# A) The trend in the change of each driver (i.e. increasing, decreasing, or complex) 
-# is shown via the coloured borders of the labelled circles. 
+# Figure XX: The socio-ecological fjord systems of the Arctic may be classified into one of five categories, 
+# with a total of 14 key drivers of change therein. 
+# A) The number of data points collected, managed, and analysed in pursuit of the understanding of each of the key drivers (log10 scale).
+# Note that there are no data points currently available for governance. 
+# B) The trend in the change of each driver (i.e. increasing, decreasing, or both) is shown 
+# via the coloured borders of the labelled circles. 
 # The impacts that the drivers have on each other are shown with coloured arrows.
-# B) The date range of data collected for each of the drivers. 
-# Note that there are no datasets/points currently available for governance.
 
