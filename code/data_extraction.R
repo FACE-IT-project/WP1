c47f08048c487421a78fe0266cdc86734dfeee3a
Robert <robert.schlegel@imev-mer.fr>
2023-01-03 09:44:46 +0000

    New script for handling/documenting extraction requests

---
+++ code/data_extraction.R
@@ -0,0 +1,23 @@
+# code/data_extraction.R
+# Herein the documentation of requested extractions from the FACE-IT dataset
+
+
+# Setup -------------------------------------------------------------------
+
+source("code/functions.R")
+
+if(!exists("clean_all")) load("data/analyses/clean_all.RData")
+
+
+# Extract -----------------------------------------------------------------
+
+# For Anäis on Jan 3, 2023
+## Kongsfjorden
+### T°, PAR, Salinity, Chla, pH
+#### depth: surface data would already be good but, if possible, data at 7/8m depth would be awesome. 
+#### period and frequency: the whole year if possible, but especially spring/summer. Weekly data would be nice.
+### how much points along the fjord: the idea of the eDNA project is to characterise the light/salinity gradient that we have along the fjord so 4 to 6 points would be awesome.
+extract_1 <- clean_all %>% 
+  filter(site == "kong",
+         driver %in% c("sea temp", "salinity", "light", "prim prod", "carb"),
+         variable %in% c("temp [°C]", "sal", "PAR [µmol m-2 s-1]", "pH [unknown scale]", "pH in situ [total scale]", "Chla [µg/l]"))
