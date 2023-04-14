6e30703fe4307e2418d4ad0bd67deaaa3cebe0bc
Robert <robert.schlegel@imev-mer.fr>
2023-04-14 11:19:25 +0200

    Beginning investigation into fjord light

---
+++ code/fjord_light.R
@@ -0,0 +1,8 @@
+# code/fjord_light.R
+# Code for exploring the data from Bernard's FjordLight package
+
+
+# Setup -------------------------------------------------------------------
+
+library(tidyverse)
+library(FjordLight) # NB: Installed directly from .tar.gz
