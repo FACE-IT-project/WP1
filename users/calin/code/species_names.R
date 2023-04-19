f7c56af63ee618c113ba3233cf6aceb19f8adbe2
Eccalin <ec.calin@gmail.com>
2023-04-19 14:56:56 +0200

    latin names formula

--- users/calin/code/species_names.R
+++ users/calin/code/species_names.R
@@ -9,6 +9,138 @@ library(tidyverse)
 
 # Function ----------------------------------------------------------------
 
+nom_latin_com2 <- function(nomSpecies){
+  # Fish
+  if(nomSpecies == "Boreogadus saida") {"Boreogadus saida (polar cod)"}
+  if(nomSpecies == "Clupea harengus") {"Clupea harengus (herring)"}
+  if(nomSpecies == "Gadus morhua") {"Gadus morhua (northeast arctic cod)"}
+  if(nomSpecies == "Mallotus villosus") {"Mallotus villosus (capelin)"}
+  if(nomSpecies == "Sebastes mentella") {"Sebastes mentella (beaked redfish)"}
+  if(nomSpecies == "Sebastes norvegicus") {"Sebastes norvegicus (golden redfish)"}
+  #Marine Mammal
+  if(nomSpecies == "Cystophora cristata") {"Cystophora cristata (hooded seal)"}
+  if(nomSpecies == "Odobenus marinus") {"Odobenus marinus (walrus)"}
+  if(nomSpecies == "Pagophilus groenlandicus") {"Pagophilus groenlandicus (harp seal)"}
+  if(nomSpecies == "Ursus maritimus") {"Ursus maritimus (polar bear)"}
+  #Seabird
+  if(nomSpecies == "Alca torda") {"Alca torda (razorbill)"}
+  if(nomSpecies == "Anas platyrhynchos") {"Anas platyrhynchos (mallard)"}
+  if(nomSpecies == "Branta Canadensis") {"Branta Canadensis (canada goose)"}
+  if(nomSpecies == "Calidris maritima") {"Calidris maritima (purple sandpiper)"}
+  if(nomSpecies == "Cepphus grylle") {"Cepphus grylle (black guillemot)"}
+  if(nomSpecies == "Clangula hyemalis") {"Clangula hyemalis (long-tailed duck)"}
+  if(nomSpecies == "Corvus corax") {"Corvus corax (raven)"}
+  if(nomSpecies == "Falco peregrinus") {"Falco peregrinus (peregrine falcon)"}
+  if(nomSpecies == "Fratercula arctica") {"Fratercula arctica (atlantic puffin)"}
+  if(nomSpecies == "Fulmarus glacialis") {"Fulmarus glacialis (northern fulmar)"}
+  if(nomSpecies == "Gavia stallata") {"Gavia stallata (red-throated loon)"}
+  if(nomSpecies == "Histrionicus histrionicus") {"Histrionicus histrionicus (harlequin duck)"}
+  if(nomSpecies == "Larus argentatus") {"Larus argentatus (herring gull)"}
+  if(nomSpecies == "Larus canus") {"Larus canus (common gull)"}
+  if(nomSpecies == "Larus delawarensis") {"Larus delawarensis (ring-billed gull)"}
+  if(nomSpecies == "Larus fuscus") {"Larus fuscus (lesser black-backed gull)"}
+  if(nomSpecies == "Larus glaucoides") {"Larus glaucoides (iceland gull)"}
+  if(nomSpecies == "Larus hyperboreus") {"Larus hyperboreus (glaucous gull)"}
+  if(nomSpecies == "Larus marinus") {"Larus marinus (great black-backed gull)"}
+  if(nomSpecies == "Lagopus mutus") {"Lagopus mutus (rock ptarmigan)"}
+  if(nomSpecies == "Larus smithonianus") {"Larus smithonianus (american herring gull)"}
+  if(nomSpecies == "Larus sp.") {"Larus sp. (gull unidentified)"}
+  if(nomSpecies == "Mergus serratus") {"Mergus serratus (red-breasted merganser)"}
+  if(nomSpecies == "Pagophila eburnea") {"Pagophila eburnea (ivory gull)"}
+  if(nomSpecies == "Phalacrocorax carbo") {"Phalacrocorax carbo (great cormorant)"}
+  if(nomSpecies == "Rissa tridactyla") {"Rissa tridactyla (black-legged kittiwake)"}
+  if(nomSpecies == "Somateria mollissima borealis") {"Somateria mollissima borealis (common eider)"}
+  if(nomSpecies == "Somateria spectabilis") {"Somateria spectabilis (king eider)"}
+  if(nomSpecies == "Uria aalge") {"Uria aalge (common guillemot)"}
+  if(nomSpecies == "Uria lomvia") {"Uria lomvia (brünnich’s guillemot)"}
+  if(nomSpecies == "Sterna paradisaea") {"Sterna paradisaea (arctic tern)"}
+  if(nomSpecies == "Stercorarius parasiticus") {"Stercorarius parasiticus (parasitic jaeger)"}
+  if(nomSpecies == "") {" ()"}
+  if(nomSpecies == "Arenaria interpres") {"Arenaria interpres (ruddy turnstone)"}
+  if(nomSpecies == "Calidris alba") {"Calidris alba (sanderling)"}
+  if(nomSpecies == "Charadrius hiaticula") {"Charadrius hiaticula (common ringed plover)"}
+  if(nomSpecies == "Gavia stellata") {"Gavia stellata (red-throated loon)"}
+  if(nomSpecies == "Calidris alpina") {"Calidris alpina (dunlin)"}
+  if(nomSpecies == "Stercorarius longicaudus") {"Stercorarius longicaudus (long-tailed jaeger)"}
+  if(nomSpecies == "Anser brachyrhynchus") {"Anser brachyrhynchus (pink-footed goose)"}
+  if(nomSpecies == "Larus hypeboreus") {"Larus hypeboreus (glaucous gull)"}
+  if(nomSpecies == "Calidris canutus") {"Calidris canutus (red knot)"}
+  if(nomSpecies == "Lagopus muta") {"Lagopus muta (rock ptarmigan)"}
+  if(nomSpecies == "Phalaropus lobatus") {"Phalaropus lobatus (red-necked phalarope)"}
+  if(nomSpecies == "Somateria mollissima") {"Somateria mollissima (common eider)"}
+  if(nomSpecies == "Bubo scandiacus") {"Bubo scandiacus (snowy owl)"}
+  if(nomSpecies == "Phalaropus fulicarius") {"Phalaropus fulicarius (red phalarope)"}
+  if(nomSpecies == "Calcarius lapponicus") {"Calcarius lapponicus (lapland longspur)"}
+  if(nomSpecies == "Plectrophenax nivalis") {"Plectrophenax nivalis (snow bunting)"}
+  if(nomSpecies == "Calidris melanotos") {"Calidris melanotos (pectoral sandpiper)"}
+  #Zooplankton
+  if(nomSpecies == "Calanus finmarchicus") {"Calanus finmarchicus (atlantic calanus (zooplankton))"}
+  if(nomSpecies == "Calanus glacialis") {"Calanus glacialis (arctic calanus (zooplankton))"}
+}
+
+
+
+testif <- function(x){ mutate(
+  
+)
+  if (x == 1) {y =="YES"} else {
+    if (x == 2) {y =="YES2"} else {
+    NA}}
+  return(y)
+}
+
+
+
+
+
+
+testif(2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -49,71 +181,5 @@ convert_UTM_deg <- function(df, utm_zone){
 
 
 # Species -----------------------------------------------------------------
-
-nom_latin_com2 <- function(nomSpecies){
-  if(nomSpecies == "Alca torda") {la_specie == "Alca torda (razorbill)"} else {
-    if(nomSpecies == "Anas platyrhynchos") {la_specie == "Anas platyrhynchos (mallard)"} else {
-      if(nomSpecies == "Boreogadus saida") {la_specie == "Boreogadus saida (polar cod)"} else {
-        if(nomSpecies == "Branta Canadensis") {la_specie == "Branta Canadensis (canada goose)"} else {
-          if(nomSpecies == "Calanus finmarchicus") {la_specie == "Calanus finmarchicus (atlantic calanus (zooplankton))"} else {
-            if(nomSpecies == "Calanus glacialis") {la_specie == "Calanus glacialis (arctic calanus (zooplankton))"} else {
-              if(nomSpecies == "Calidris maritima") {la_specie == "Calidris maritima (purple sandpiper)"} else {
-                if(nomSpecies == "Cepphus grylle") {la_specie == "Cepphus grylle (black guillemot)"} else {
-                  if(nomSpecies == "Clupea harengus") {la_specie == "Clupea harengus (herring)"} else {
-                    if(nomSpecies == "Clangula hyemalis") {la_specie == "Clangula hyemalis (long-tailed duck)"} else {
-                      if(nomSpecies == "Corvus corax") {la_specie == "Corvus corax (raven)"} else {
-                        if(nomSpecies == "Cystophora cristata") {la_specie == "Cystophora cristata (hooded seal)"} else {
-                          if(nomSpecies == "Falco peregrinus") {la_specie == "Falco peregrinus (peregrine falcon)"} else {
-                            if(nomSpecies == "Fratercula arctica") {la_specie == "Fratercula arctica (atlantic puffin)"} else {
-                              if(nomSpecies == "Fulmarus glacialis") {la_specie == "Fulmarus glacialis (northern fulmar)"} else {
-                                if(nomSpecies == "Gadus morhua") {la_specie == "Gadus morhua (northeast arctic cod)"} else {
-                                  if(nomSpecies %int% c("Gavia stallata", "Gavia stellata")) {la_specie == "Gavia stallata (red-throated loon)"} else {
-                                    if(nomSpecies == "Histrionicus histrionicus") {la_specie == "Histrionicus histrionicus (harlequin duck)"} else {
-                                      if(nomSpecies == "Larus argentatus") {la_specie == "Larus argentatus (herring gull)"} else {
-                                        if(nomSpecies == "Larus canus") {la_specie == "Larus canus (common gull)"} else {
-                                          if(nomSpecies == "Larus delawarensis") {la_specie == "Larus delawarensis (ring-billed gull)"} else {
-                                            if(nomSpecies == "Larus fuscus") {la_specie == "Larus fuscus (lesser black-backed gull)"} else {
-                                              if(nomSpecies == "Larus glaucoides") {la_specie == "Larus glaucoides (iceland gull)"} else {
-                                                if(nomSpecies == "Larus hyperboreus") {la_specie == "Larus hyperboreus (glaucous gull)"} else {
-                                                  if(nomSpecies == "Larus marinus") {la_specie == "Larus marinus (great black-backed gull)"} else {
-                                                    if(nomSpecies == "Lagopus mutus") {la_specie == "Lagopus mutus (rock ptarmigan)"} else {
-                                                      if(nomSpecies == "Larus smithonianus") {la_specie == "Larus smithonianus (american herring gull)"} else {
-                                                        if(nomSpecies == "Larus sp.") {la_specie == "Larus sp. (gull unidentified)"} else {
-                                                          if(nomSpecies == "Mallotus villosus") {la_specie == "Mallotus villosus (capelin)"} else {
-                                                            if(nomSpecies == "Mergus serratus") {la_specie == "Mergus serratus (red-breasted merganser)"} else {
-                                                              if(nomSpecies == "Odobenus marinus") {la_specie == "Odobenus marinus (walrus)"} else {
-                                                                if(nomSpecies == "Pagophila eburnea") {la_specie == "Pagophila eburnea (ivory gull)"} else {
-                                                                  if(nomSpecies == "Pagophilus groenlandicus") {la_specie == "Pagophilus groenlandicus (harp seal)"} else {
-                                                                    if(nomSpecies == "Phalacrocorax carbo") {la_specie == "Phalacrocorax carbo (great cormorant)"} else {
-                                                                      if(nomSpecies == "Rissa tridactyla") {la_specie == "Rissa tridactyla (black-legged kittiwake)"} else {
-                                                                        if(nomSpecies == "Sebastes mentella") {la_specie == "Sebastes mentella (beaked redfish)"} else {
-                                                                          if(nomSpecies == "Sebastes norvegicus") {la_specie == "Sebastes norvegicus (golden redfish)"} else {
-                                                                            if(nomSpecies == "Somateria mollissima borealis") {la_specie == "Somateria mollissima borealis (common eider)"} else {
-                                                                              if(nomSpecies == "Somateria spectabilis") {la_specie == "Somateria spectabilis (king eider)"} else {
-                                                                                if(nomSpecies == "Uria aalge") {la_specie == "Uria aalge (common guillemot)"} else {
-                                                                                  if(nomSpecies == "Uria lomvia") {la_specie == "Uria lomvia (brünnich’s guillemot)"} else {
-                                                                                    if(nomSpecies == "Ursus maritimus") {la_specie == "Ursus maritimus (polar bear)"} else {
-                                                                                      if(nomSpecies == "Sterna paradisaea") {la_specie == "Sterna paradisaea (arctic tern)"} else {
-                                                                                        if(nomSpecies == "Stercorarius parasiticus") {la_specie == "Stercorarius parasiticus (parasitic jaeger)"} else {
-                                                                                          if(nomSpecies == "") {la_specie == " ()"} else {
-                                                                                            if(nomSpecies == "Arenaria interpres") {la_specie == "Arenaria interpres (ruddy turnstone)"} else {
-                                                                                              if(nomSpecies == "Calidris alba") {la_specie == "Calidris alba (sanderling)"} else {
-                                                                                                if(nomSpecies == "Charadrius hiaticula") {la_specie == "Charadrius hiaticula (common ringed plover)"} else {
-                                                                                                  if(nomSpecies == "Gavia stellata") {la_specie == "Gavia stellata (red-throated loon)"} else {
-                                                                                                    if(nomSpecies == "Calidris alpina") {la_specie == "Calidris alpina (dunlin)"} else {
-                                                                                                      if(nomSpecies == "Stercorarius longicaudus") {la_specie == "Stercorarius longicaudus (long-tailed jaeger)"} else {
-                                                                                                        if(nomSpecies == "Anser brachyrhynchus") {la_specie == "Anser brachyrhynchus (pink-footed goose)"} else {
-                                                                                                          if(nomSpecies == "Larus hypeboreus") {la_specie == "Larus hypeboreus (glaucous gull)"} else {
-                                                                                                            if(nomSpecies == "Calidris canutus") {la_specie == "Calidris canutus (red knot)"} else {
-                                                                                                              if(nomSpecies == "Lagopus muta") {la_specie == "Lagopus muta (rock ptarmigan)"} else {
-                                                                                                                if(nomSpecies == "Phalaropus lobatus") {la_specie == "Phalaropus lobatus (red-necked phalarope)"} else {
-                                                                                                                  if(nomSpecies == "Somateria mollissima") {la_specie == "Somateria mollissima (common eider)"} else {
-                                                                                                                    if(nomSpecies == "Bubo scandiacus") {la_specie == "Bubo scandiacus (snowy owl)"} else {
-                                                                                                                      if(nomSpecies == "Phalaropus fulicarius") {la_specie == "Phalaropus fulicarius (red phalarope)"} else {
-                                                                                                                        if(nomSpecies == "Calcarius lapponicus") {la_specie == "Calcarius lapponicus (lapland longspur)"} else {
-                                                                                                                          if(nomSpecies == "Plectrophenax nivalis") {la_specie == "Plectrophenax nivalis (snow bunting)"} else {
-                                                                                                                            if(nomSpecies == "Calidris melanotos") {la_specie == "Calidris melanotos (pectoral sandpiper)"}
-                                                                                                                            return(la_specie)
-}
 
 
