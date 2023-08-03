aa6b5bf512d8e0354ef9e90d55508e5a33c09a87
Robert William Schlegel <robwschlegel@gmail.com>
2023-08-03 10:48:14 +0200

    Working on extracting current+speed data for WP4

--- code/gridded_analysis.R
+++ code/gridded_analysis.R
@@ -5,8 +5,10 @@
 # Setup -------------------------------------------------------------------
 
 # Libraries
+# devtools::install_github("harphub/meteogrid")
 library(tidyverse)
 library(tidync)
+library(meteogrid) # For converting USpeed+UDir to u+v
 library(doParallel); registerDoParallel(cores = 15)
 
 # Find who is the user and define the pCloud path
@@ -16,11 +18,17 @@ if (Sys.getenv("LOGNAME") == "gattuso"){
   pCloud_path = "~/pCloudDrive/" 
 }
 
+# Remove scientific notation
+options(scipen=999)
+
 
 # Data --------------------------------------------------------------------
 
+# Metadata
 ncdump::NetCDF(paste0(pCloud_path,"FACE-IT_data/kongsfjorden/kongsfjorden_rcp2.6.nc"))
 var_names <- ncdump::NetCDF(paste0(pCloud_path,"FACE-IT_data/kongsfjorden/kongsfjorden_rcp2.6.nc"))$variable
+
+# Data
 depth_data <- tidync(paste0(pCloud_path,"FACE-IT_data/kongsfjorden/kongsfjorden_rcp2.6.nc")) %>% 
   hyper_tibble()
 surface_data <- tidync(paste0(pCloud_path,"FACE-IT_data/kongsfjorden/kongsfjorden_rcp2.6.nc")) %>% 
@@ -66,3 +74,35 @@ all_data %>%
   labs(x = NULL, y = "Temperature (°C)", colour = "depth (m)")
 ggsave("figures/test_ts.png", height = 4, width = 8)
 
+
+# Extract current data for is and por -------------------------------------
+
+is_date <- tidync("~/pCloudDrive/FACE-IT_data/model/isfjorden_rcp8.5.nc") |> 
+  activate("D2") |> 
+  hyper_tibble() |> 
+  mutate(t = as.Date(as.POSIXct(T*3600, origin = "1950-01-01 01:00"))) 
+is_meta <- tidync("~/pCloudDrive/FACE-IT_data/model/isfjorden_rcp8.5.nc") |> 
+  activate("D0,D1") |> 
+  hyper_tibble() 
+is_data <- tidync("~/pCloudDrive/FACE-IT_data/model/isfjorden_rcp8.5.nc") |> 
+  hyper_filter(T = between(T, 438731, 648227)) |> 
+  hyper_tibble() 
+is_current <- is_data |> 
+  left_join(is_date, by = "T") |> 
+  left_join(is_meta, by = c("X", "Y")) |> 
+  filter(RMask == 1) |> 
+  dplyr::select(Long, Latt, Topo, Z, t, Udir, USpeed) |> 
+  mutate(wind.uv(USpeed, Udir))
+
+is_current %>%
+  filter(t == "2000-01-19", Z == 0) |> 
+  ggplot(aes(x = Long, y = Latt)) + 
+  geom_segment(aes(xend = Long + U, yend = Latt + V, colour = USpeed), linewidth = 2,
+               arrow = arrow(angle = 15, length = unit(0.2, "cm"), type = "open")) +
+  borders(colour = "black", size = 1) +
+  scale_colour_viridis_c() +
+  coord_cartesian(xlim = range(is_current$Long), ylim = range(is_current$Latt)) +
+  labs(y = "Latitude (°N)", x = "Longitude (°E)", colour = "Current\nspeed [m/s]    ") +
+  theme(panel.border = element_rect(fill = NA, colour = "black"),
+        legend.position = "bottom", legend.key.width = unit(1, "cm"))
+ggsave("figures/is_surface.png", height = 6, width = 6)
