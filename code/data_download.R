5026e9c66ad18aae3e364f0fd3aceedd923707ec
Robert <robert.schlegel@imev-mer.fr>
2024-02-14 07:53:46 +0000

    Some preliminary work on the kelp barcode project.

--- code/data_download.R
+++ code/data_download.R
@@ -39,12 +39,29 @@ taxon_list <- c("Bangiophyceae", "Florideophyceae", "Phaeophyceae", "Ulvophyceae
 
 # Get them all in one go
 # NB: The download is extremely slow...but don't run them in parallel
-bold1 <- refdb_import_BOLD("Xanthophyceae", ncbi_taxo = FALSE)
+bold1 <- refdb_import_BOLD("Xanthophyceae", ncbi_taxo = FALSE, full = TRUE)
+ncbi1 <- refdb_import_NCBI(taxon_list[5], full = TRUE)
 # NB: One of these BOLD taxa calls has an error in a column that prevents the internal rbind from working
 # Running all of these at once is too time consuming/error prone
 # Better to run and save each one individually
-bold_algae <- plyr::ldply(taxon_list, refdb_import_BOLD, .parallel = FALSE, ncbi_taxo = FALSE)
-ncbi_algae <- plyr::ldply(taxon_list, refdb_import_NCBI, .parallel = FALSE)
+## That being said, on 2024-02-12 the full BOLD query managed to run
+bold_algae <- plyr::ldply(taxon_list, refdb_import_BOLD, .parallel = FALSE, ncbi_taxo = FALSE, full = TRUE)
+
+# The same goes for GenBank
+ncbi_algae <- plyr::ldply(taxon_list, refdb_import_NCBI, .parallel = FALSE, full = TRUE, seq_bin = 1000)
+ncbi_ban <- refdb_import_NCBI(taxon_list[1], full = TRUE, seq_bin = 2000)
+ncbi_flo <- refdb_import_NCBI(taxon_list[1], full = TRUE, seq_bin = 2000)
+ncbi_pha <- refdb_import_NCBI(taxon_list[1], full = TRUE, seq_bin = 2000)
+ncbi_ulv <- refdb_import_NCBI(taxon_list[1], full = TRUE, seq_bin = 2000)
+ncbi_xan <- refdb_import_NCBI(taxon_list[1], full = TRUE, seq_bin = 2000)
+
+# Save
+write_csv(bold_algae, "~/pCloudDrive/FACE-IT_data/barcode/bold_algae.csv")
+write_csv(ncbi_algae, "~/pCloudDrive/FACE-IT_data/barcode/ncbi_algae.csv")
+
+# Or try a location
+bold_arctic <- refdb_import_BOLD(geo = "Arctic", ncbi_taxo = FALSE)
+ncbi_arctic <- refdb_import_NCBI("Arctic", full = TRUE)
 
 # Filter to rough Arctic circle
 # NB: Roughly 80% of GenBank records have no lon/lat
@@ -56,3 +73,28 @@ ncbi_algae_Arctic <- filter(ncbi_algae, latitude >= 60)
 write_csv(bold_algae_Arctic, "~/pCloudDrive/FACE-IT_data/barcode/bold_algae_Arctic.csv")
 write_csv(ncbi_algae_Arctic, "~/pCloudDrive/FACE-IT_data/barcode/ncbi_algae_Arctic.csv")
 
+# Load files for analysis below if needed
+if(!exists("bold_algae")) bold_algae <- read_csv("~/pCloudDrive/FACE-IT_data/barcode/bold_algae.csv")
+if(!exists("bold_algae_Arctic")) bold_algae_Arctic <- read_csv("~/pCloudDrive/FACE-IT_data/barcode/bold_algae_Arctic.csv")
+
+# Check number of BOLD records with no lon/lat coords
+filter(bold_algae, is.na(lon) | is.na(lat)) |> nrow()
+filter(bold_algae_Arctic, lat >= 66) |> nrow()
+
+# Extract just unique records per location
+# Not currently interested in quantifying the multiples
+bold_Arctic_unq <- bold_algae_Arctic |> 
+  dplyr::select(phylum_name, class_name, order_name, family_name, genus_name, species_name, lon, lat, country) |> 
+  distinct()
+bold_high_Arctic_unq <- bold_Arctic_unq |> filter(lat >= 66)
+
+# Create map of records
+ggplot(bold_Arctic_unq, aes(x = lon, y = lat)) + 
+  borders() +
+  geom_point(aes(colour = class_name), size = 5, alpha = 0.7,
+             position = position_dodge(width = 5.0)) +
+  coord_quickmap(ylim = c(59, 81), xlim = c(-172, 30)) +
+  labs(x = NULL, y = NULL) +
+  theme(legend.position = "bottom",
+        panel.border = element_rect(fill = NA, colour = "black"))
+ggsave("~/pCloudDrive/FACE-IT_data/barcode/bold_Arctic_unq_map.png", height = 3, width = 7)
