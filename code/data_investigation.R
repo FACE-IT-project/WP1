8d6e9348a3b80b0da13841531f3ba2450a3660bc
Robert William Schlegel <robwschlegel@gmail.com>
2024-03-11 21:17:50 +0000

    Some initial thoughts into the final data investigation

---
+++ code/data_investigation.R
@@ -0,0 +1,21 @@
+# data_investigation.R
+# This script investigates what is in the final clean dataset
+
+
+# Setup -------------------------------------------------------------------
+
+# Libraries used in this script
+source("code/functions.R")
+
+
+# Load data ---------------------------------------------------------------
+
+
+# Visuals -----------------------------------------------------------------
+
+# Horizontal boxplots of temperature etc. by depth with box colour showing annual trend
+
+# List of variable names. Perhaps time series or scatter plot.
+
+# Barplot time series of complete monthly daily data. But how for depth? 
+
